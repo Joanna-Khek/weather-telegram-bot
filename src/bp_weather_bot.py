8b6e4c6bf00652bd3f5c8e04365802f6a6c540c9
Joanna Khek <joannakhek@gmail.com>
2023-02-12 15:25:26 +0800

    updated image path url

--- src/bp_weather_bot.py
+++ src/bp_weather_bot.py
@@ -23,16 +23,16 @@ def send_message():
     time_period = f'*{start_time} 到 {end_time}*'
 
     if (status == "Light Showers" or status == "Showers"):
-        image_path = open("../images/light_shower.png", "rb")
-        msg = date + "\n" + time_period + "\n" + "Bukit Panjang" + "\n" + '会下雨' + "\n" + f' {status}'
+        image_path = "https://raw.githubusercontent.com/Joanna-Khek/weather-telegram-bot/main/images/light_shower.png"
+        msg = date + "\n" + time_period + "\n" + "*Bukit Panjang*" + "\n" + '会下雨' + "\n" + f' {status}'
         bot.send_photo(chat_id=BP_CHAT_ID, photo=image_path, caption=msg, parse_mode=telegram.ParseMode.MARKDOWN)
     elif status == 'Thundery Showers':
-        image_path = open("../images/thundery_shower.png", "rb")
-        msg = date + "\n" + time_period + "\n" + "Bukit Panjang" + "\n" +'会下大雨' + "\n" + f' {status}'
+        image_path = "https://raw.githubusercontent.com/Joanna-Khek/weather-telegram-bot/main/images/thundery_shower.png"
+        msg = date + "\n" + time_period + "\n" + "*Bukit Panjang*" + "\n" +'会下大雨' + "\n" + f' {status}'
         bot.send_photo(chat_id=BP_CHAT_ID, photo=image_path, caption=msg, parse_mode=telegram.ParseMode.MARKDOWN)
     elif status == "Light Rain":
-        image_path = open("../images/light_rain.png", "rb")
-        msg = date + "\n" + time_period + "\n" + "Bukit Panjang" + "\n" +'会下小雨' + "\n" + f' {status}'
+        image_path = "https://raw.githubusercontent.com/Joanna-Khek/weather-telegram-bot/main/images/light_rain.png"
+        msg = date + "\n" + time_period + "\n" + "*Bukit Panjang*" + "\n" +'会下小雨' + "\n" + f' {status}'
         bot.send_photo(chat_id=BP_CHAT_ID, photo=image_path, caption=msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
 # filter
